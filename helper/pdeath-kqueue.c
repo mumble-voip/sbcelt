cc5df5ac73a442ba71200fe0fdcc5649b9df7ccc
Mikkel Krautz <mikkel@krautz.dk>
2012-08-02 23:10:58 +0200

    helper: fix pdeath-kqueue; add various pdeath tests.

--- helper/pdeath-kqueue.c
+++ helper/pdeath-kqueue.c
@@ -6,15 +6,36 @@
 #include <sys/event.h>
 #include <sys/time.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <pthread.h>
+#include <errno.h>
+
+#include "eintr.h"
+
+static int kqfd = -1;
 
 static void *pdeath_monitor(void *udata) {
-	int kqfd = *((int *)udata);
+	(void) udata;
 	while (1) {
 		struct kevent ke;
-		if (kevent(kqfd, NULL, 0, &ke, 1, NULL) == 0) {
+		int ret = HANDLE_EINTR(kevent(kqfd, NULL, 0, &ke, 1, NULL));
+		if (ret == 0) {
+			// No events for us.
+			continue;
+		} else if (ret > 0) {
+			// Our parent has died.
 			_exit(0);
+		} else if (ret == -1) {
+			// Most of kevent()'s returned errors are programmer errors,
+			// except ENOMEM and ESRCH.
+			//
+			// In case of ENOMEM, we might as well exit.  It's not clear
+			// when ESRCH is returned, but the racy case of the parent
+			// going away before we set up our kevent struct is handled
+			// by the pdeath function below, so it should be safe to ignore.
+			if (errno == ENOMEM) {
+				_exit(50);
+			}
+			return NULL;
 		}
 	}
 	return NULL;
@@ -23,7 +44,6 @@ static void *pdeath_monitor(void *udata) {
 int pdeath() {
 	struct kevent ke;
 	pid_t ppid;
-	int kqfd;
 
 	kqfd = kqueue();
 	if (kqfd == -1) {
@@ -36,12 +56,16 @@ int pdeath() {
 	}
 
 	EV_SET(&ke, ppid, EVFILT_PROC, EV_ADD, NOTE_EXIT, 0, NULL);
-	if (kevent(kqfd, &ke, 1, NULL, 0, NULL) == -1) {
+	if (HANDLE_EINTR(kevent(kqfd, &ke, 1, NULL, 0, NULL)) == -1) {
+		// Parent seemingly already dead.
+		if (errno == ESRCH) {
+			_exit(0);
+		}
 		return -1;
 	}
 
 	pthread_t thr;
-	if (pthread_create(&thr, NULL, pdeath_monitor, &kqfd) == -1) {
+	if (pthread_create(&thr, NULL, pdeath_monitor, NULL) == -1) {
 		return -1;
 	}
 
---
+++ test/pdeath/pdeath_test.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "pdeath.h"
+#include "mtime.h"
+#include "eintr.h"
+
+int main(int argc, char *argv[]) {
+	pid_t pid = fork();
+	if (pid == 0) {
+		// Close parent's fds.
+		HANDLE_EINTR(close(0));
+		HANDLE_EINTR(close(1));
+		HANDLE_EINTR(close(2));
+
+		// Don't check return value.
+		// If pdeath fails it's hard to signal to the parent,
+		// and exitting will signal that pdeath *worked*.
+		pdeath();
+		while (1) {
+			usleep(USEC_PER_SEC*1);
+		}
+	} else {
+		usleep(USEC_PER_SEC*1);
+		printf("%lu\n", (unsigned long)pid);
+	}
+	return 0;
+}
